e0f07785c5bdc7dfbc617352b0e9e6ced17aea5f
Dulguun Battulga <dolugen@users.noreply.github.com>
2020-04-15 13:50:26 +0200

    Hapi v17 support

--- index.js
+++ index.js
@@ -3,74 +3,71 @@
 
 var _ = require('lodash');
 
-exports.register = function (server, options, next) {
-  var defaultPage = 1;
-  var defaultLimit = options.limit || 100;
-  var name = options.name || 'meta';
-  var results = options.results || 'results';
-  var routes = options.routes || ['*'];
-  var excludeFormats = options.excludeFormats || [];
-  var requestLimit = defaultLimit;
-  var requestPage = defaultPage;
+exports.plugin = {
+  pkg: require('./package.json'),
+  register: function (server, options) {
+    var defaultPage = 1;
+    var defaultLimit = options.limit || 100;
+    var name = options.name || 'meta';
+    var results = options.results || 'results';
+    var routes = options.routes || ['*'];
+    var excludeFormats = options.excludeFormats || [];
+    var requestLimit = defaultLimit;
+    var requestPage = defaultPage;
 
-  server.ext('onPreHandler', function (request, reply) {
-    if (_.has(request.query, 'page')) {
-      requestPage = _.parseInt(request.query.page);
-      request.query = _.omit(request.query, 'page');
-    } else {
-      requestPage = defaultPage;
-    }
-
-    if (_.has(request.query, 'limit')) {
-      requestLimit = _.parseInt(request.query.limit);
-      request.query = _.omit(request.query, 'limit');
-    } else {
-      requestLimit = defaultLimit;
-    }
-
-    request.page = requestPage;
-    request.limit = requestLimit;
-
-    return reply.continue();
-  });
-
-  server.ext('onPreResponse', function (request, reply) {
-    var meta = {
-      page: requestPage,
-      limit: request.limit
-    };
-
-    if (_.has(request, 'count')) {
-      meta.found = request.count;
-    }
-
-    // Make sure route matches and we're not exclude based on format
-    if ((routes.indexOf(request.route.path) !== -1 || routes[0] === '*') &&
-      excludeFormats.indexOf(request.query.format) === -1) {
-      if (_.has(request.response.source, name)) {
-        request.response.source[name] = _.merge(request.response.source[name], meta);
+    server.ext('onPreHandler', function (request, h) {
+      if (_.has(request.query, 'page')) {
+        requestPage = _.parseInt(request.query.page);
+        request.query = _.omit(request.query, 'page');
       } else {
-        // Because we want to add meta to top of the source, we have to go through all this hastle
-        var temp = request.response.source;
-        request.response.source = {};
-        request.response.source[name] = meta;
-        request.response.source[results] = temp;
+        requestPage = defaultPage;
       }
-    } else {
-      // Remove any previous meta content since we don't want it in this case
-      if (_.has(request.response.source, name)) {
-        delete request.response.source[name].page;
-        delete request.response.source[name].limit;
-        delete request.response.source[name].found;
+
+      if (_.has(request.query, 'limit')) {
+        requestLimit = _.parseInt(request.query.limit);
+        request.query = _.omit(request.query, 'limit');
+      } else {
+        requestLimit = defaultLimit;
       }
-    }
 
-    return reply.continue();
-  });
+      request.page = requestPage;
+      request.limit = requestLimit;
 
-  next();
-};
+      return h.continue;
+    });
 
-exports.register.attributes = {
-    pkg: require('./package.json')
+    server.ext('onPreResponse', function (request, h) {
+      var meta = {
+        page: requestPage,
+        limit: request.limit
+      };
+
+      if (_.has(request, 'count')) {
+        meta.found = request.count;
+      }
+
+      // Make sure route matches and we're not exclude based on format
+      if ((routes.indexOf(request.route.path) !== -1 || routes[0] === '*') &&
+        excludeFormats.indexOf(request.query.format) === -1) {
+        if (_.has(request.response.source, name)) {
+          request.response.source[name] = _.merge(request.response.source[name], meta);
+        } else {
+          // Because we want to add meta to top of the source, we have to go through all this hastle
+          var temp = request.response.source;
+          request.response.source = {};
+          request.response.source[name] = meta;
+          request.response.source[results] = temp;
+        }
+      } else {
+        // Remove any previous meta content since we don't want it in this case
+        if (_.has(request.response.source, name)) {
+          delete request.response.source[name].page;
+          delete request.response.source[name].limit;
+          delete request.response.source[name].found;
+        }
+      }
+
+      return h.continue;
+    });
+  }
 };
--- test/test.js
+++ test/test.js
@@ -1,116 +1,96 @@
 /* global require, describe, it */
 'use strict';
 
-var Hapi = require('hapi');
+var Hapi = require('@hapi/hapi');
 var expect = require('chai').expect;
 
 var register = function () {
   var server = new Hapi.Server();
-  server.connection();
-  server.route({ method: 'GET', path: '/', handler: function (request, reply) { return reply('ok'); }});
+  server.route({ method: 'GET', path: '/', handler: function (request, h) { return 'ok'; }});
 
   return server;
 };
 
 describe('hapi-test', function () {
-  it('test if limit is added to request', function (done) {
+  it('test if limit is added to request', async function (done) {
     var server = register();
-    server.register(require('../'), function (err) {
-      expect(err).to.be.empty;
-
-      var request = { method: 'GET', url: '/'};
-      server.inject(request, function (res) {
-        expect(res.request.limit).to.equal(100);
-        done();
-      });
-    });
+    await server.register(require('../'));
+    var request = { method: 'GET', url: '/'};
+    var res = await server.inject(request);
+    expect(res.request.limit).to.equal(100);
+    done();
   });
 
-  it('test if page is added to request', function (done) {
+  it('test if page is added to request', async function (done) {
     var server = register();
-    server.register(require('../'), function (err) {
-      expect(err).to.be.empty;
-
-      var request = { method: 'GET', url: '/'};
-      server.inject(request, function (res) {
-        expect(res.request.page).to.equal(1);
-        done();
-      });
-    });
+    await server.register(require('../'));
+    var request = { method: 'GET', url: '/'};
+    var res = await server.inject(request);
+    expect(res.request.page).to.equal(1);
+    done();
   });
 
-  it('test if page and limit are captured from query params', function (done) {
+  it('test if page and limit are captured from query params', async function (done) {
     var server = register();
-    server.register(require('../'), function (err) {
-      expect(err).to.be.empty;
+    await server.register(require('../'));
 
-      // call page and limit
-      var request = { method: 'GET', url: '/?page=3&limit=10'};
-      server.inject(request, function (res) {
-        expect(res.request.page).to.equal(3);
-        expect(res.request.limit).to.equal(10);
-      });
+    // call page and limit
+    var request = { method: 'GET', url: '/?page=3&limit=10'};
+    var res = await server.inject(request);
+    expect(res.request.page).to.equal(3);
+    expect(res.request.limit).to.equal(10);
 
-      // call again withtout them and make sure page is back to default value
-      var request = { method: 'GET', url: '/'};
-      server.inject(request, function (res) {
-        expect(res.request.page).to.equal(1);
-        expect(res.request.limit).to.equal(100);
-      });
+    // call again withtout them and make sure page is back to default value
+    var request = { method: 'GET', url: '/'};
+    var res = await server.inject(request);
+    expect(res.request.page).to.equal(1);
+    expect(res.request.limit).to.equal(100);
 
-      // call again with them and see if correct value is returned
-      var request = { method: 'GET', url: '/?page=4&limit=11'};
-      server.inject(request, function (res) {
-        expect(res.request.page).to.equal(4);
-        expect(res.request.limit).to.equal(11);
-        done();
-      });
-    });
+    // call again with them and see if correct value is returned
+    var request = { method: 'GET', url: '/?page=4&limit=11'};
+    var res = await server.inject(request);
+    expect(res.request.page).to.equal(4);
+    expect(res.request.limit).to.equal(11);
+    done();
   });
 
-  it('test response output', function (done) {
+  it('test response output', async function (done) {
     var server = register();
-    server.register(require('../'), function (err) {
-      expect(err).to.be.empty;
+    await server.register(require('../'));
 
-      var request = { method: 'GET', url: '/'};
-      server.inject(request, function (res) {
-        var output = {
-          meta: {
-            page: 1,
-            limit: 100
-          },
-          results: 'ok'
-        };
-        expect(res.result).to.have.all.keys(output);
-        done();
-      });
-    });
+    var request = { method: 'GET', url: '/'};
+    var res = await server.inject(request);
+    var output = {
+      meta: {
+        page: 1,
+        limit: 100
+      },
+      results: 'ok'
+    };
+    expect(res.result).to.have.all.keys(output);
+    done();
   });
 
-  it('test with options', function (done) {
+  it('test with options', async function (done) {
     var server = register();
     var plugin = {
-      register: require('../'),
+      plugin: require('../'),
       options: {
         limit: 1000,
         name: 'Some Name',
         results: 'output'
       }
     };
-    server.register(plugin, function (err) {
-      expect(err).to.be.empty;
+    await server.register(plugin);
 
-      var request = { method: 'GET', url: '/'};
-      server.inject(request, function (res) {
-        expect(res.request.limit).to.equal(1000);
-        expect(res.result).to.have.all.keys(['Some Name', 'output']);
-        done();
-      });
-    });
+    var request = { method: 'GET', url: '/'};
+    var res = await server.inject(request);
+    expect(res.request.limit).to.equal(1000);
+    expect(res.result).to.have.all.keys(['Some Name', 'output']);
+    done();
   });
 
-  it('test when meta already exist', function (done) {
+  it('test when meta already exist', async function (done) {
     var server = register();
 
     var output = {
@@ -121,21 +101,18 @@ describe('hapi-test', function () {
       results: 'ok'
     };
 
-    server.route({ method: 'GET', path: '/new', handler: function (request, reply) { return reply(output); }});
-    server.register(require('../'), function (err) {
-      expect(err).to.be.empty;
+    server.route({ method: 'GET', path: '/new', handler: function (request, h) { return output; }});
+    await server.register(require('../'));
 
-      var request = { method: 'GET', url: '/new'};
-      server.inject(request, function (res) {
-        output.meta.page = 1;
-        output.meta.limit = 100;
-        expect(res.result).to.have.all.keys(output);
-        done();
-      });
-    });
+    var request = { method: 'GET', url: '/new'};
+    var res = await server.inject(request);
+    output.meta.page = 1;
+    output.meta.limit = 100;
+    expect(res.result).to.have.all.keys(output);
+    done();
   });
 
-  it('test route option', function (done) {
+  it('test route option', async function (done) {
     var server = register();
 
     var output = {
@@ -149,76 +126,68 @@ describe('hapi-test', function () {
     server.route({
       method: 'GET',
       path: '/with',
-      handler: function (request, reply) {
-        return reply(output);
+      handler: function (request, h) {
+        return output;
       }
     });
     server.route({
       method: 'GET',
       path: '/without',
-      handler: function (request, reply) {
-        return reply({this: 'that'});
+      handler: function (request, h) {
+        return {this: 'that'};
       }
     });
     server.route({
       method: 'GET',
       path: '/with_meta',
-      handler: function (request, reply) {
-        return reply({
+      handler: function (request, h) {
+        return {
           meta: {
             important: 'yes'
           },
           results: {
             this: 'that'
           }
-        });
+        };
       }
     });
-    server.register({
-      register: require('../'),
+    await server.register({
+      plugin: require('../'),
       options: {
         routes: ['/with']
       }
-    }, function (err) {
-      expect(err).to.be.empty;
-
-      var request = { method: 'GET', url: '/with'};
-      server.inject(request, function (res) {
-        expect(res.result.meta).to.have.any.keys('page', 'limit');
-      });
-
-      var request = { method: 'GET', url: '/without'};
-      server.inject(request, function (res) {
-        expect(res.result).to.not.have.any.keys('meta');
-      });
-
-      var request = { method: 'GET', url: '/with_meta'};
-      server.inject(request, function (res) {
-        expect(res.result.meta).to.not.have.any.keys('page', 'limit');
-        done();
-      });
     });
+
+    var request = { method: 'GET', url: '/with'};
+    var res = await server.inject(request);
+    expect(res.result.meta).to.have.any.keys('page', 'limit');
+
+    var request = { method: 'GET', url: '/without'};
+    var res = await server.inject(request);
+    expect(res.result).to.not.have.any.keys('meta');
+
+    var request = { method: 'GET', url: '/with_meta'};
+    var res = await server.inject(request);
+    expect(res.result.meta).to.not.have.any.keys('page', 'limit');
+    done();
   });
 
-  it('test exclude option', function (done) {
+  it('test exclude option', async function (done) {
     var server = register();
     var plugin = {
-      register: require('../'),
+      plugin: require('../'),
       options: {
         limit: 1000,
         name: 'Some Name',
         excludeFormats: ['csv']
       }
     };
-    server.register(plugin, function (err) {
-      expect(err).to.be.empty;
+    await server.register(plugin);
 
-      var request = { method: 'GET', url: '/?format=csv'};
-      server.inject(request, function (res) {
-        expect(res.result).to.equal('ok');
-        done();
-      });
-    });
+    var request = { method: 'GET', url: '/?format=csv'};
+    var res = await server.inject(request);
+    expect(res.result).to.equal('ok');
+    done();
   });
 
 });
